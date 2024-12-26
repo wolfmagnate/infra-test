ee3af5d40a36775ff58b221e5ab71e9ad296948f
進捗ゼミ <47470998+wolfmagnate@users.noreply.github.com>
2024-12-26 19:58:50 +0900

    feat: add backend

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # infra-test
 infra test for AI Friendly Framework
+
+## Directory
+
+## Frontend
+- /code/frontend
+
+## Backend
+- /code/backend
+
+
+1. Dockerfileを普通に作成
+
+結局のところ、CMDとEXPOSEを設定して起動すればOK
+
+2. GitHub Actionsでbuild
+
+3. Artifact Registryにpush
+
+golang
---
+++ code/backend/main.go
@@ -0,0 +1,33 @@
+// Sample run-helloworld using Echo
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	// Create a new Echo instance
+	e := echo.New()
+
+	// Define the handler for the root route
+	e.GET("/", func(c echo.Context) error {
+		name := os.Getenv("NAME")
+		if name == "" {
+			name = "World"
+		}
+		return c.String(http.StatusOK, "Hello "+name+"!\n")
+	})
+
+	// Determine port for HTTP service
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	// Start server
+	e.Logger.Printf("Listening on port %s", port)
+	e.Logger.Fatal(e.Start(":" + port))
+}
